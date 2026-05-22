09d7e8a49d6503b24337049c2c7377513cb5d642
Максим Немчинов <ivanfilgerovitch@gmail.com>
2026-05-23 04:08:33 +0500

    added colour palette, added night theme for android, fixed bug with buttons on top of each other

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pipka"
+    namespace = "com.example.Flavor"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.pipka"
+        applicationId = "com.example.Flavor"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "pipka"
+rootProject.name = "Flavor"
 include(":app")
